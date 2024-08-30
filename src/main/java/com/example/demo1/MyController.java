d9bf921ffcd7ff6040002bb52532857a77e9ffbd
HamzaMerdassi55 <hamzamerdassi41@gmail.com>
2024-08-30 13:28:15 +0100

    first commit

---
+++ src/main/java/com/example/demo1/MyController.java
@@ -0,0 +1,20 @@
+package com.example.demo1;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+@RestController
+public class MyController {
+    private final Counter myCounter;
+    public MyController(MeterRegistry registry) {
+        this.myCounter = Counter.builder("my_custom_counter")
+                .description("A custom counter metric")
+                .register(registry);
+    }
+    @GetMapping("/my-endpoint")
+    public String myEndpoint() {
+// Increment the counter each time this endpoint is called
+        myCounter.increment();
+        return "<h1>عليكم السالم</h1>";
+    }
+}
